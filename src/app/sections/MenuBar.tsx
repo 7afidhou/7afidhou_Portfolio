8bdd521400ffbd83141512d4ca515f53d8356861
7afidhou <abdelhafidhhaouari2@gmail.com>
2025-06-29 11:39:35 +0100

    m

---
+++ src/app/sections/MenuBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function MenuBar() {
+  return (
+    <div className='text-secondary flex justify-between mx-5 mt-5'>
+      7afidhou
+      <div className='flex gap-4'>
+        <a href="">Home</a>
+        <a href="">Contact Me</a>
+        <a href="">About</a>
+      </div>
+    </div>
+  )
+}
